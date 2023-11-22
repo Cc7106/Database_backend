6a0ec7f6fe815265ec48a7b749bf6a105c4d22c8
Cheryl <jchye22@gmail.com>
2023-11-22 08:07:42 +0800

    Initialized

--- src/main/java/com/example/demo/API/UserController.java
+++ src/main/java/com/example/demo/API/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("http://localhost:3000/")
 @RequestMapping( "/demo")
 public class UserController {
     @Autowired
