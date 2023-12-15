41c59ba3db8220774b7e1daadfdbb9a28f1633bf
Cheryl <jchye22@gmail.com>
2023-12-16 05:30:08 +0800

    add filter review

--- src/main/java/com/example/demo/Dao/ReviewRepository.java
+++ src/main/java/com/example/demo/Dao/ReviewRepository.java
@@ -8,7 +8,15 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.ArrayList;
 
 public interface ReviewRepository extends CrudRepository<Review, String> {
-    @Query("select r from Review r where r.booking.id = :bookingId")
+    @Query("select r from Review r where r.booking.id = :bookingId order by r.booking.dateToCollect")
     Review findReviewByBooking(String bookingId);
 
+    @Query("select r from Review r where r.booking.car.carModel.name LIKE %:carModelName% order by r.booking.dateToCollect")
+    ArrayList<Review> findReviewByCarModelName(String carModelName);
+
+    @Query("select r from Review r where r.rating = :stars order by r.booking.dateToCollect")
+    ArrayList<Review> findReviewByRating(int stars);
+
+    @Query("select r from Review r where r.booking.car.carModel.carMake.name = :carMake order by r.booking.dateToCollect")
+    ArrayList<Review> findReviewByCarMake(String carMake);
 }
